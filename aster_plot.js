2251cf6910412fcfb9ea599d7c21935488d89027
David Szajngarten <David@David-Szajngarten-2.local>
2019-05-20 18:31:11 +0100

    added legend, custom keyword search option for centre metric, leveraging config.color arrays from input.

--- aster_plot.js
+++ aster_plot.js
@@ -10,20 +10,20 @@ looker.plugins.visualizations.add({
       display: "radio",
       default: "large"
     },
-    color: {
-      type: 'string',
-      label: 'Custom Color',
-      display: 'color',
+    color_range: {
+      type: "array",
+      label: "Color Range",
+      display: "colors"
     },
-    diameter: {
-      type: "string",
-      label: "Diameter",
-      default: '100%',
-      placeholder: "100%"
+    radius: {
+      type: "number",
+      label: "Circle Radius",
+      default: 100,
+      placeholder: 100
     },
     keyword_search: {
       type: "string",
-      label: "Enter Keyword to search for",
+      label: "Enter custom keyword to search for",
       placeholder: "Power"
     }
   },
@@ -97,6 +97,13 @@ looker.plugins.visualizations.add({
       top: 100%;
       left: 0;
     }
+
+    .legend rect {
+      fill:white;
+      stroke:black;
+      opacity:0.8;
+    }
+
       </style> `;
 
     element.innerHTML = css;
@@ -109,7 +116,7 @@ looker.plugins.visualizations.add({
 
   // Render in response to the data or settings changing
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    this.container.innerHTML = '' // clear container of previous vis so width & height is correct
+    this.container.innerHTML = '' // clear container of previous vis
     this.clearErrors(); // clear any errors from previous updates
 
     // ensure data fit - requires no pivots, exactly 1 dimension_like field, and exactly 2 measure_like fields
@@ -122,8 +129,6 @@ looker.plugins.visualizations.add({
 
     var dimension = queryResponse.fields.dimension_like[0].name;
     var measure1 = queryResponse.fields.measure_like[0].name, measure2 = queryResponse.fields.measure_like[1].name;
-    
-    console.log(data)
 
     var width = element.clientWidth,
       height = element.clientHeight,
@@ -146,7 +151,7 @@ looker.plugins.visualizations.add({
     var arc = d3.svg.arc()
       .innerRadius(innerRadius)
       .outerRadius(function(d) {
-        return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+        return (radius - innerRadius) * (d.data.score / (1.0*config.radius)) + innerRadius;
       });
 
     var outlineArc = d3.svg.arc()
@@ -161,22 +166,61 @@ looker.plugins.visualizations.add({
 
     svg.call(tip);
 
+    if (!config.color_range) {
+      config.color_range = ["#62bad4", "#a9c574", "#929292", "#9fdee0", "#1f3e5a", "#90c8ae", "#92818d", "#c5c6a6", "#82c2ca", "#cee0a0", "#928fb4", "#9fc190"];
+    }
 
-    data.forEach(function(d) {
-      // console.log(d)
-      // d.id = d['aster_plot_custom_vis.id'].value;
-      // d.order = +d.order;
-      d.label = d[dimension].value;
-      d.color = d['aster_plot_custom_vis.color'].value; // handle in vis config tab
-      d.score = +d[measure1].value; // length of slice (circle is 100)
-      d.weight = +d[measure2].value; // angle of slice (width of slice)
-      d.width = +d[measure2].value; // angle of slice (width of slice)
-    });
+    var color_length = config.color_range.length;
+    for (let i = 0; i < data.length; i++) {
+      if (i >= color_length) {
+        let j = Math.floor(i/color_length)
+        data[i].color = config.color_range[i-(j*color_length)];
+      } else {
+        data[i].color = config.color_range[i];
+      }
+      data[i].label = data[i][dimension].value; // dimension label
+      data[i].score = +data[i][measure1].value; // length of slice (circle radius default is 100)
+      data[i].weight = +data[i][measure2].value; // angle of slice (width of slice)
+      data[i].width = +data[i][measure2].value; // angle of slice (width of slice)      
+    }
+
+    // calculate the weighted mean score (value in centre of pie)
+    if (!config.keyword_search) {
+      var score =
+        data.reduce(function(a, b) {
+          console.log('Default weighted mean score')
+          //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
+          return a + (b.score * b.weight);
+        }, 0) /
+        data.reduce(function(a, b) {
+          return a + b.weight;
+        }, 0);
+    } else {
+      // custom keyword option
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].label.toLowerCase().includes(config.keyword_search.toLowerCase())) {
+          console.log(data[i].label + ' is used for centre score');
+          var score = data[i].score;
+          data.splice(i,1)
+          break;
+        }
+      }
+    }
+
+    console.log(data)
+
+    // affix score to centre of pie
+    svg.append("svg:text")
+      .attr("class", "aster-score")
+      .attr("dy", ".35em")
+      .attr("text-anchor", "middle") // text-align: right
+      .text(Math.round(score));
 
 
     var path = svg.selectAll(".solidArc")
       .data(pie(data))
       .enter().append("path")
+      .attr("data-legend",function(d) { return d.data.label}) // legend
       .attr("fill", function(d) {
         return d.data.color;
       })
@@ -194,22 +238,13 @@ looker.plugins.visualizations.add({
       .attr("class", "outlineArc")
       .attr("d", outlineArc);
 
+    // legend
+    var legend = svg.append("g")
+      .attr("class","legend")
+      .attr("transform","translate(-" + width/2.2 + " ,-" + height/2.5 + ")")
+      .style("font-size","12px")
+      .call(d3legend)
 
-    // calculate the weighted mean score
-    var score =
-      data.reduce(function(a, b) {
-        //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
-        return a + (b.score * b.weight);
-      }, 0) /
-      data.reduce(function(a, b) {
-        return a + b.weight;
-      }, 0);
-
-    svg.append("svg:text")
-      .attr("class", "aster-score")
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle") // text-align: right
-      .text(Math.round(score));
 
 
     // Helper functions
@@ -242,6 +277,64 @@ looker.plugins.visualizations.add({
           && check('dim-req', 'Dimension', dimensions.length, options.min_dimensions, options.max_dimensions)
           && check('mes-req', 'Measure', measures.length, options.min_measures, options.max_measures));
     }
+
+
+    // Legend
+    // (C) 2012 ziggy.jonsson.nyc@gmail.com
+    // MIT licence
+    function d3legend(g) {
+      g.each(function() {
+        var g= d3.select(this),
+            items = {},
+            svg = d3.select(g.property("nearestViewportElement")),
+            legendPadding = g.attr("data-style-padding") || 5,
+            lb = g.selectAll(".legend-box").data([true]),
+            li = g.selectAll(".legend-items").data([true])
+
+        lb.enter().append("rect").classed("legend-box",true)
+        li.enter().append("g").classed("legend-items",true)
+
+        svg.selectAll("[data-legend]").each(function() {
+            var self = d3.select(this)
+            items[self.attr("data-legend")] = {
+              pos : self.attr("data-legend-pos") || this.getBBox().y,
+              color : self.attr("data-legend-color") != undefined ? self.attr("data-legend-color") : self.style("fill") != 'none' ? self.style("fill") : self.style("stroke") 
+            }
+          })
+
+        items = d3.entries(items).sort(function(a,b) { return a.value.pos-b.value.pos})
+
+        
+        li.selectAll("text")
+            .data(items,function(d) { return d.key})
+            .call(function(d) { d.enter().append("text")})
+            .call(function(d) { d.exit().remove()})
+            .attr("y",function(d,i) { return i+"em"})
+            .attr("x","1em")
+            .text(function(d) { ;return d.key})
+        
+        li.selectAll("circle")
+            .data(items,function(d) { return d.key})
+            .call(function(d) { d.enter().append("circle")})
+            .call(function(d) { d.exit().remove()})
+            .attr("cy",function(d,i) { return i-0.25+"em"})
+            .attr("cx",0)
+            .attr("r","0.4em")
+            .style("fill",function(d) { 
+              // console.log(d.value.color);
+              return d.value.color
+            })  
+        
+        // Reposition and resize the box
+        var lbbox = li[0][0].getBBox()  
+        lb.attr("x",(lbbox.x-legendPadding))
+            .attr("y",(lbbox.y-legendPadding))
+            .attr("height",(lbbox.height+2*legendPadding))
+            .attr("width",(lbbox.width+2*legendPadding))
+      })
+      return g
+    }
+
 
     done()
   }
